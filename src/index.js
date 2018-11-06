9fea31b80e8c26e437785cff5673c07025d1fa33
Mason Fox <Fox@Masons-MacBook-Pro.local>
2018-11-06 16:13:18 -0700

    This is a commit message - this is now awesome

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+    response.end('This is Awesome');
 }).listen(8000);
 
 console.log('Server running at http://127.0.0.1:8000/');
